afaf1045b981abd7a509bee36293b1022a49c2f2
DominicDev <s0l0estudi4r@gmail.com>
2026-05-23 23:42:20 +0200

    v0.0.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import React, { useEffect, useState } from "react";
-import Header from "./components/Header";
-import InputBox from "./components/Inputbox";
-import ExtraOptions from "./components/ExtraOptions";
-import OutputBox from "./components/OutputBox";
-import "./App.css";
+import { useEffect, useState } from 'react';
+import Header from './components/Header';
+import InputBox from './components/Inputbox';
+import ExtraOptions from './components/ExtraOptions';
+import OutputBox from './components/OutputBox';
+import './App.css';
 
 function App() {
-  const [temp, setTemp] = useState("");
-  const [word, setWord] = useState("");
+  const [temp, setTemp] = useState('');
+  const [word, setWord] = useState('');
   const [size, setSize] = useState(200);
-  const [bgColor, setBgColor] = useState("ffffff");
-  const [qrCode, setQrCode] = useState("");
+  const [bgColor, setBgColor] = useState('ffffff');
+  const [qrCode, setQrCode] = useState('');
 
-  // Changing the URL only when the user changes the input
   useEffect(() => {
     setQrCode(
       `http://api.qrserver.com/v1/create-qr-code/?data=${word}!&size=${size}x${size}&bgcolor=${bgColor}`
     );
   }, [word, size, bgColor]);
 
-  // Updating the input word when user clicks on the generate button
   function handleClick() {
     setWord(temp);
   }
 
   return (
-    <div className="flex flex-col items-center gap-12 pt-8">
+    <div className='flex flex-col items-center gap-12 pt-8'>
       <Header />
-      <div className="flex flex-col items-center gap-4">
+      <div className='flex flex-col items-center gap-4'>
         <InputBox setTemp={setTemp} handleClick={handleClick} />
         <ExtraOptions setBgColor={setBgColor} size={size} setSize={setSize} />
       </div>
